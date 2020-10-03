05101024a05a84f4a8a0277f265de6a4d2b8447e
José Andrés Ceballos Vadillo <andres@ceballos.dev>
2020-10-02 20:13:36 -0500

    Adding comments

--- ejercicio1.6.py
+++ ejercicio1.6.py
@@ -1,5 +1,20 @@
+# Programa que lee un código postal desde teclado, lo busca dentro
+# de un archivo json y en caso de encontrarse el código imprime
+# el estado y municipio al que pertenece el código. En caso de que no
+# se encuentre imprime: "Código Postal NO encontrado".
+# 
+# Tecnológico Nacional de México Campus Colima
+# Ingeniería en Sistemas Computacionales
+# Lenguaje de programación Python
+#
+# José Andrés Ceballos Vadillo
+# 17460386
+
+
+# Importar libreria para trabajar con json
 import json
 
+# Método para solicitar código postal y validar formato
 def get_postal_code():
     while True:
         code = input("Ingrese el código postal a buscar: ")
@@ -13,6 +28,7 @@ def get_postal_code():
             print("El código postal se debe componer de 5 dígitos")
     return code
 
+# Método para abrir json e iterar sus elementos en busqueda de código postal
 def find_postal_code(code, json_file):
     with open(json_file, encoding="utf8") as file:
         data = json.load(file)
